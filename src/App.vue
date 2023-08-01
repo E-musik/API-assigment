551fca9b769f384c209c466f9b7b0ffdfb3d1d32
E-musik <emmamusik888@gmail.com>
2023-08-01 05:15:44 +0100

    APIassignment

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div>
+   <h1>Movie</h1>
+    <div class="input">
+      <input type="search" v-model="search">
+      <button @click="getposts">search</button>
+    </div>
+
+    <div class="result">
+      <ul>
+        <li v-for="post in post" :key="post">{{ post }}</li>
+      </ul>
+    
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return{
+      post: [],
+      search:''
+    }
+  },
+  methods: {
+    getposts(){
+      fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=96b56e3b`)
+      .then(Response => Response.json())
+      .then(data => this.post = data)
+    }
   }
-}
+};
+
 </script>
 
 <style>
@@ -19,8 +39,12 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.result{
+  background-color: rgb(87, 149, 190);
 }
 </style>
